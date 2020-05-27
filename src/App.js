53b169a4c13ad575dbad7a047ea8d32476ef11b5
Allen Chen <allchen@expedia.com>
2020-05-26 23:35:01 -0700

    added new modal to replace the sidebar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import HeaderMobile from './components/mobile/header-mobile';
 import PartyOrdersMobile from './components/mobile/party-orders-mobile';
 import PhotoCarousel from './components/photo-carousel';
 import AboutUs from './components/about-us';
-import SlidingPane from 'react-sliding-pane';
-import 'react-sliding-pane/dist/react-sliding-pane.css';
+import Modal from 'react-awesome-modal';
 import './App.css';
 
 class App extends Component {
@@ -40,7 +39,23 @@ class App extends Component {
             </div>
           </div>
         )}
-        <SlidingPane
+        <Modal visible={this.state.isBottomDialogOpen} effect="fadeInUp" onClickAway={ () => {
+            this.setState({ isBottomDialogOpen: false });
+          }}>
+          <div className="mobile-popout">
+            <div className="popout-address">
+              <address>
+                42 Springstowne Ctr<br/>
+                Vallejo, CA 94591<br/>
+              </address>
+              <div className="popout-maps-link" onClick={this.openMobileMaps}>Open in maps <span role="img" aria-label="emoji maps">📲</span></div>
+            </div>
+            <div className="phone-button-wrapper">
+              <a href="tel:707-648-1066" className="mobile-popout-number"><img src="https://res.cloudinary.com/ac31624/image/upload/v1590560559/combo/phone_icon.png"  className="mobile-popout-phone-icon" /><span id='phone-number'>707-648-1066</span></a>
+            </div>
+          </div>
+        </Modal>
+        {/* <SlidingPane
           className='mobile-popout'
           overlayClassName='mobile-popout-overlay'
           isOpen={ this.state.isBottomDialogOpen }
@@ -61,7 +76,7 @@ class App extends Component {
             <div className="party-order-message">Please call the number below to place an order, thank you!</div>
             <div className="mobile-popout-number"><span role="img"><span role="img" aria-label="emoji phone">📞</span> : </span><a href="tel:707-648-1066">707-648-1066</a></div>
           </div>
-        </SlidingPane>
+        </SlidingPane> */}
       </>
     )
   }
@@ -83,6 +98,7 @@ class App extends Component {
           <PartyOrdersMobile />
           <div className="mobile-divider"><img className="mobile-divider-image" src="https://res.cloudinary.com/ac31624/image/upload/v1554081007/combo/chopsticks_rotated_b1ft0x.png" alt="chopsticks divider"/></div>     
           {this.renderMobileContactDialog()}
+          <img  alt="combo restaurant front" className="popout-image" src="http://res.cloudinary.com/ac31624/image/upload/c_scale,w_832/v1554845685/combo/combo_front_hd6mi2.jpg" />
         </MediaQuery>
       </div>
     );
